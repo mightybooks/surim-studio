6caa99fe5002e8056eb0aecff592897c699d48f9
Mun Surim <surimstudio@gmail.com>
2026-01-15 00:24:06 +0900

    testcard12

--- src/app/shop/testcard/[slug]/page.tsx
+++ src/app/shop/testcard/[slug]/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
   const product = {
     type: "BOOK",
     name: "수리미 쩐다요",
-    price: 100,
+    price: 1100,
     delivery: "택배 / 1~3영업일",
     thumbnail: "/productlist/tosiltosil_list.webp",
     detailImage: "/productdetail/tosiltosil_detail.webp",
